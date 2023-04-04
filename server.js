77377e34411f9ca122b22db38c264a29438a16f1
savaj <savaj@users.noreply.github.com>
2023-04-04 19:52:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "This is a test practical." });
+});
+
+require("./app/routes/sales.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
